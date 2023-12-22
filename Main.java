acdb3da3dd5d5423a677563da5f3e2a77f0c0cde
Arnold C <78853864+arnoldcuriano@users.noreply.github.com>
2023-12-22 15:20:40 +0800

    Java Programming Activity 1

---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
+/*The error is missing "" on System.out.println / arnoldC!~*/
